9ad0dd886b4cdcbf70f97ef44ec351cc92f46fea
Carla-DV <carla.divirgilio15@gmail.com>
2022-11-08 11:56:22 -0300

    commit2

--- calcu.js
+++ calcu.js
@@ -1,26 +1,24 @@
- let numero1 = prompt("Ingrese numero 1");
+ let numero1 = prompt("Ingrese un numero");
  let numero2 = prompt("Ingrese otro numero");
- let operacion = prompt("Ingrese la operacion matematica");
+ let operacion = prompt("Ingrese una operacion matematica: +, -, *, /");
  let resultado;
+ 
  if (operacion == "+"){
     resultado = numero1 + numero2;
+    alert("El resultado es: "+ resultado)
  }else if (operacion == "-"){
     resultado = numero1 - numero2;
+    alert("El resultado es: "+ resultado)
  }else if (operacion == "*"){
    resultado = numero1 * numero2;
+   alert("El resultado es: "+ resultado)
 }else if (operacion == "/"){
    resultado = numero1 / numero2;
+   alert("El resultado es: "+ resultado)
+}else if(operador == "/"){
+   total = numero1/(0)
+   alert("ERROR: No se puede dividir por 0")
+}else {
+   alert("ERROR")
 }
-
-//  //+
-//  let suma= numero1+numero2;
-// //-
-// let resta= numero1-numero2
-// //*
-// let multiplicacion= numero1*numero2
-// // /
-// let divsion= numero1/numero2
-
-console.log ("El resultado es:" + resultado);
-alert ("error")
 
